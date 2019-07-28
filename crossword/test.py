e19e509d0605d56fc75e8e325e09f04719cecbb8
DESKTOP-9NDNENB\pablo <peropaquepo@gmail.com>
2019-07-29 00:11:21 +0200

    check of pre existent id in generation siblings

---
+++ crossword/test.py
@@ -0,0 +1,90 @@
+class Test:
+    def __init__(self, value):
+        self.value = value
+        self.parents = []
+        self.children = []
+
+    def spawn(self):
+        new_one = Test(self.value+1)
+        new_one.parents += [self]
+        self.children += [new_one]
+        return new_one
+
+    def print_parents_values(self):
+        print('parents value: ')
+        for parent in self.parents:
+            print(parent.value)
+
+    def print_children_values(self):
+        print('children value: ')
+        for child in self.children:
+            print(child.value)
+
+import pandas as pd
+import numpy as np
+from copy import copy
+
+class Crossword:
+
+    def __init__(self, current, word_indexes, crossing_ids=[]):
+        self.current = current
+        self.parents = []
+        self.children = []
+        self.word_indexes = word_indexes
+        self.crossing_ids = crossing_ids
+
+    #def check_legit_crossing(self, current):
+
+    def is_child_already_in_gen(self, crossing_id, generation):
+        child_id = self.crossing_ids + [crossing_id]
+        for i, sibling in enumerate(generation):
+            if len(sibling.crossing_ids) == len(child_id):
+                if set(sibling.crossing_ids) == set(child_id):
+                    return i
+        return None
+
+    def spawn(self, new_word, word_index, unique_crossings, generation):
+        if word_index in self.word_indexes:
+            return None
+
+        # Prepare possible crossings
+        possible_crossings = pd.merge(self.current, new_word, how='inner',
+                                on='letter', suffixes=('_current', '_new'))
+
+        # Iterate over possible crossings
+        print(possible_crossings)
+        for i, crossing in possible_crossings.iterrows():
+
+            # Identify crossing
+            on = ['word_index_first', 'letter_index_first',
+                       'word_index_second', 'letter_index_second']
+
+            row_df = pd.Series()
+            if crossing['word_index_current'] < crossing['word_index_new']:
+                row_df['word_index_first'] = crossing['word_index_current']
+                row_df['letter_index_first'] = crossing['letter_index_current']
+                row_df['word_index_second'] = crossing['word_index_new']
+                row_df['letter_index_second'] = crossing['letter_index_new']
+            else:
+                row_df['word_index_first'] = crossing['word_index_new']
+                row_df['letter_index_first'] = crossing['letter_index_new']
+                row_df['word_index_second'] = crossing['word_index_current']
+                row_df['letter_index_second'] = crossing['letter_index_current']
+            row_df = pd.DataFrame(row_df).T
+            #print(i)
+            #print(crossing)
+            #print(row_df)
+            crossing_id = pd.merge(unique_crossings, row_df,
+                                   how='inner', on=on)['crossing_id'].iloc[0]
+
+            # Check if possible child crossing id is already in generation
+            pre_existent_child = self.is_child_already_in_gen(crossing_id,
+                                                              generation)
+            if pre_existent_child:
+                # If so, add child to this node children,
+                self.children += [pre_existent_child]
+                # And add this node as a parent for the children node
+                pre_existent_child.parents += [self]
+                continue
+
+            # Check if crossing is legit
---
+++ main.py
@@ -0,0 +1,62 @@
+from crossword.test import Crossword
+
+import pandas as pd
+import numpy as np
+from copy import copy
+
+
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 200)
+
+original_words = sorted(['MAMA', 'PAPA', 'ABUELA', 'PERRO'])
+
+words = []
+for i, word in enumerate(original_words):
+    word_df = pd.DataFrame()
+    for j, letter in enumerate(word):
+        row = pd.Series()
+        row['letter'] = letter
+        row['letter_index'] = j
+        word_df = word_df.append(row, ignore_index=True)
+    word_df['word_index'] = i
+    #word_df['word'] = word
+    word_df.letter_index = word_df.letter_index.astype(int)
+    words += [word_df]
+#print(words)
+
+
+# Unique id crossings stored in a dataframe
+unique_crossings = pd.DataFrame()
+
+for i in range(len(words)):
+    for j in range(i+1, len(words)):
+        cross = pd.merge(words[i], words[j], how='inner', on='letter',
+                         suffixes=('_first', '_second'))
+        unique_crossings = unique_crossings.append(cross)
+unique_crossings['crossing_id'] = np.arange(unique_crossings.shape[0],
+                                            dtype=int)
+
+#print(unique_crossings)
+
+crosswords = {0: []}
+for word_index, word in enumerate(words):
+    cw = Crossword(word, [word_index])
+    crosswords[0] += [cw]
+
+for layer in range(1, len(words)):
+    # Initialize empty layer
+    generation = []
+
+    # Reproduce parents
+    for parent_cw in crosswords[layer-1]:
+        for word_index, word in enumerate(words):
+            if word_index==0:continue
+            child = parent_cw.spawn(word, word_index, unique_crossings,
+                                    generation)
+            #if child is None:
+            #    continue
+            break
+        break
+    break
+    # Save the current generation
+    crosswords[layer] = copy(generation)

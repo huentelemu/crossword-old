5ae72aa550809efa2f86fbcfa774e757330cda19
DESKTOP-9NDNENB\pablo <peropaquepo@gmail.com>
2019-08-04 23:38:14 +0200

    finished exhaustive search method

--- crossword/test.py
+++ crossword/test.py
@@ -1,3 +1,7 @@
+import pandas as pd
+import numpy as np
+
+
 class Test:
     def __init__(self, value):
         self.value = value
@@ -20,9 +24,6 @@ class Test:
         for child in self.children:
             print(child.value)
 
-import pandas as pd
-import numpy as np
-from copy import copy
 
 class Crossword:
 
@@ -32,52 +33,64 @@ class Crossword:
         self.children = []
         self.word_indexes = word_indexes
         self.crossing_ids = crossing_ids
-
+        self.left_on = ['word_index_first', 'letter_index_first',
+                        'word_index_second', 'letter_index_second']
+        self.right_on = ['word_index_current', 'letter_index_current',
+                         'word_index_new', 'letter_index_new']
+        self.right_on_inverse = ['word_index_new', 'letter_index_new',
+                                 'word_index_current', 'letter_index_current']
     #def check_legit_crossing(self, current):
 
     def is_child_already_in_gen(self, crossing_id, generation):
-        child_id = self.crossing_ids + [crossing_id]
+        child_id = self.crossing_ids + crossing_id
         for i, sibling in enumerate(generation):
             if len(sibling.crossing_ids) == len(child_id):
                 if set(sibling.crossing_ids) == set(child_id):
-                    return i
+                    return generation[i]
         return None
 
     def spawn(self, new_word, word_index, unique_crossings, generation):
         if word_index in self.word_indexes:
             return None
 
+        new_children = []
         # Prepare possible crossings
         possible_crossings = pd.merge(self.current, new_word, how='inner',
                                       on='letter', suffixes=('_current', '_new'))
 
-        # Iterate over possible crossings
-        print(new_word)
-        print(possible_crossings)
+        # Figure out start coordinates of the new word in each crossing
+        possible_crossings['new_word_start_x'] = 0
+        possible_crossings['new_word_start_y'] = 0
+        possible_crossings['new_word_horizontal'] = 0
         for i, crossing in possible_crossings.iterrows():
-
-            # Identify crossing
-            on = ['word_index_first', 'letter_index_first',
-                       'word_index_second', 'letter_index_second']
-
-            row_df = pd.Series()
-            if crossing['word_index_current'] < crossing['word_index_new']:
-                row_df['word_index_first'] = crossing['word_index_current']
-                row_df['letter_index_first'] = crossing['letter_index_current']
-                row_df['word_index_second'] = crossing['word_index_new']
-                row_df['letter_index_second'] = crossing['letter_index_new']
+            if crossing.horizontal:
+                possible_crossings.loc[i, 'new_word_horizontal'] = False
+                possible_crossings.loc[i, 'new_word_start_x'] = crossing.x
+                possible_crossings.loc[i, 'new_word_start_y'] = crossing.y - crossing.letter_index_new
             else:
-                row_df['word_index_first'] = crossing['word_index_new']
-                row_df['letter_index_first'] = crossing['letter_index_new']
-                row_df['word_index_second'] = crossing['word_index_current']
-                row_df['letter_index_second'] = crossing['letter_index_current']
-            row_df = pd.DataFrame(row_df).T
+                possible_crossings.loc[i, 'new_word_horizontal'] = True
+                possible_crossings.loc[i, 'new_word_start_x'] = crossing.x - crossing.letter_index_new
+                possible_crossings.loc[i, 'new_word_start_y'] = crossing.y
 
-            crossing_id = pd.merge(unique_crossings, row_df,
-                                   how='inner', on=on)['crossing_id'].iloc[0]
+        # Iterate over each possible position of the new word
+        grouped_crossings = possible_crossings.groupby(['new_word_start_x',
+                                                        'new_word_start_y',
+                                                        'new_word_horizontal'])
+        for nw_coords, group in grouped_crossings:
+
+            # Identify the ID/s of this specific crossing
+            if group.word_index_current.iloc[0] < group.word_index_new.iloc[0]:
+                crossing_df = pd.merge(unique_crossings, group, how='inner',
+                                        left_on=self.left_on,
+                                        right_on=self.right_on)
+            else:
+                crossing_df = pd.merge(unique_crossings, group, how='inner',
+                                        left_on=self.left_on,
+                                        right_on=self.right_on_inverse)
 
             # Check if possible child crossing id is already in generation
-            pre_existent_child = self.is_child_already_in_gen(crossing_id,
+            crossing_ids = crossing_df['crossing_id'].tolist()
+            pre_existent_child = self.is_child_already_in_gen(crossing_ids,
                                                               generation)
             if pre_existent_child:
                 # If so, add child to this node children,
@@ -86,26 +99,60 @@ class Crossword:
                 pre_existent_child.parents += [self]
                 continue
 
-            # Create new word
+            # This is a new position to explore then, let's create a new word
+            inserted_new_word = new_word.copy()
+            start = {'x': nw_coords[0], 'y': nw_coords[1]}
+            horizontal = nw_coords[2]
+            fix_axis = ['x', 'y'][horizontal]
+            along_axis = ['x', 'y'][not horizontal]
             len_word = new_word.shape[0]
-            nw = new_word.copy()
-            if crossing.horizontal:
-                fix_axis = 'x'
-                along_axis = 'y'
-                nw['horizontal'] = False
-            else:
-                fix_axis = 'y'
-                along_axis = 'x'
-                nw['horizontal'] = True
-            nw[fix_axis] = crossing[fix_axis]
-            start_word = crossing[along_axis] - crossing.letter_index_new
-            nw[along_axis] = np.arange(start_word, start_word+len_word)
-            print(nw)
+            inserted_new_word[fix_axis] = start[fix_axis]
+            inserted_new_word[along_axis] = np.arange(start[along_axis],
+                                                      start[along_axis]+len_word)
+            inserted_new_word['horizontal'] = horizontal
 
-            # Check if crossing is legit
-            #new_cw =
+            # Create area that should not merge with any element of the current crossword
+            fa = start[fix_axis]
+            aa = start[along_axis]
+            # Forbid start tip
+            forbidden_area_dict = [{fix_axis: fa, along_axis: aa-1}]
+            # Forbid borders along the new word
+            for i, coord in enumerate(inserted_new_word[along_axis]):
+                # Except in places where we cross with the current crossword
+                if coord in crossing_df[along_axis].tolist():
+                    continue
+                forbidden_area_dict += [{fix_axis: fa-1, along_axis: coord},
+                                        {fix_axis: fa, along_axis: coord},
+                                        {fix_axis: fa+1, along_axis: coord}]
+            # Forbid end tip
+            forbidden_area_dict += [{fix_axis: fa, along_axis: aa+len_word}]
 
+            # Merge forbidden area to current crossword to check if word fits
+            forbidden_area = pd.DataFrame(forbidden_area_dict)
+            n_forbidden_contacts = pd.merge(self.current, forbidden_area).shape[0]
 
-            break
+            if n_forbidden_contacts > 0:
+                # Word doesn't fit like this, so we just continue with the next position
+                continue
 
+            # We are ready to spawn a new child crossword with the new word
+            new_crossword = self.current.copy()
+            new_crossword = new_crossword.append(inserted_new_word, sort=False)
 
+            # Erase the letters involved in the current crossing
+            crossing_letters = np.logical_and(new_crossword.x.isin(crossing_df.x),
+                                              new_crossword.y.isin(crossing_df.y))
+            new_crossword = new_crossword.loc[~crossing_letters]
+
+            # Create child
+            child_id = self.crossing_ids + crossing_ids
+            child_word_indexes = self.word_indexes + [word_index]
+            new_child = Crossword(new_crossword, child_word_indexes, child_id)
+
+            # Assign family to child
+            self.children += [new_child]
+            new_child.parents += [self]
+
+            new_children += [new_child]
+
+        return new_children
--- main.py
+++ main.py
@@ -3,12 +3,39 @@ from crossword.test import Crossword
 import pandas as pd
 import numpy as np
 from copy import copy
+from time import perf_counter
 
 
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 200)
 
 original_words = sorted(['MAMA', 'PAPA', 'ABUELA', 'PERRO'])
+#original_words = sorted(['AMA','AMA','AMA'])
+#original_words = ['DRAMA',
+#         'DESOCUPAR',
+#         'PENALIDAD',
+#         'CARGO',
+#         'INVADIR',
+#         'PROFESION',
+#         'LIBERAR',
+#         'OCUPACION',
+#         'ADUENARSE',
+#         'COMUNICADO',
+#         'APROPIARSE',
+#         'INSTALARSE',
+#         'QUEHACER',
+#         'TRABAJO',
+#         'DESHABITAR',
+#         'INVASION',
+#         'VIVIR',
+#         'EMPLEO',
+#         'DEDICARSE',
+#         'FUNCION',
+#         'HABITAR',
+#         'TRAGEDIA',
+#         'ASALTO',
+#         'DOCUMENTO',
+#         ]
 
 words = []
 for i, word in enumerate(original_words):
@@ -36,31 +63,46 @@ for i in range(len(words)):
 unique_crossings['crossing_id'] = np.arange(unique_crossings.shape[0],
                                             dtype=int)
 
-#print(unique_crossings)
+print(unique_crossings)
+init_total = perf_counter()
 
+# Create set of first crosswords with only one word each
 crosswords = {0: []}
 for word_index, word in enumerate(words):
     one_word_cw = word.copy()
     one_word_cw['horizontal'] = True
-    one_word_cw['x'] = 0
-    one_word_cw['y'] = np.arange(word.shape[0])
+    one_word_cw['x'] = np.arange(word.shape[0])
+    one_word_cw['y'] = 0
     cw = Crossword(one_word_cw, [word_index])
     crosswords[0] += [cw]
 
+print('')
+print('Layer: 0')
+print('Number of crosswords in generation: '+str(len(crosswords[0])))
+
+# Create sets of crosswords with consecutively more words in them
 for layer in range(1, len(words)):
+    init = perf_counter()
     # Initialize empty layer
     generation = []
 
     # Reproduce parents
     for parent_cw in crosswords[layer-1]:
         for word_index, word in enumerate(words):
-            if word_index==0:continue
-            child = parent_cw.spawn(word, word_index, unique_crossings,
-                                    generation)
-            #if child is None:
-            #    continue
-            break
-        break
-    break
+            children = parent_cw.spawn(word, word_index,
+                                       unique_crossings,
+                                       generation)
+            if children is None:
+                continue
+
+            generation += children
+
     # Save the current generation
     crosswords[layer] = copy(generation)
+    print('')
+    print('Layer: ' + str(layer))
+    print('Number of crosswords in generation: ' + str(len(generation)))
+    print('Time: '+str(perf_counter()-init))
+
+print('')
+print('Total time: '+str(perf_counter()-init_total))
